2f48d0594fd12376a62fba7a00442d1768903bbc
Zela2520 <sugarbeatzela@gmail.com>
2022-11-08 04:20:13 +0300

    hw-2 done

--- QA/QA/urls.py
+++ QA/QA/urls.py
@@ -1,21 +1,15 @@
-"""QA URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('question_id/<int:question_id>', views.question_page, name='question_page'),
+    path('hot/', views.hot, name='hot'),
+    path('ask/', views.ask, name='ask'),
+    path('settings/', views.settings, name='settings'),
+    path('tag/', views.tag, name='tag'),
+    path('authorization/', views.authorization, name='authorization'),
+    path('registration/', views.registration, name='registration')
 ]
--- QA/app/views.py
+++ QA/app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def question_page(request, question_id: int):
+    return render(request, 'question_page.html')
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def hot(request):
+    return render(request, 'hot.html')
+
+
+def tag(request):
+    return render(request, 'tag.html')
+
+
+def settings(request):
+    return render(request, 'settings.html')
+
+
+def authorization(request):
+    return render(request, 'authorization.html')
+
+
+def registration(request):
+    return render(request, 'registration.html')
